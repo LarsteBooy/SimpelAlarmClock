f1db1b996576d49d043760e664223fe8bf76364e
LarsteBooy <36959162+LarsteBooy@users.noreply.github.com>
2018-07-25 20:15:42 +0200

    A simpel AlarmClock
    the alarm features user input and events

---
+++ AlarmClock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Timers;
+
+namespace AlarmClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            
+            Console.WriteLine("enter a number of seconds... ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            Timer aTimer = new Timer();
+            aTimer.Interval = number * 1000;
+
+            aTimer.Elapsed += OnTimedEvent;
+
+            aTimer.Enabled = true;
+            Console.ReadLine();
+        }
+        
+        private static void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("The threshold was reached at {0}", e.SignalTime);
+            Environment.Exit(0);
+        }
+    }
+}
